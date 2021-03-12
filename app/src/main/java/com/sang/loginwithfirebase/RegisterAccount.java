280170b05e415e2cc7926645059a6199a5ff1853
SANG NGUYEN DINH <sangnguyendinh24work@gmail.com>
2021-03-12 11:14:15 +0700

    Login With Firebase

--- app/src/main/java/com/sang/loginwithfirebase/RegisterAccount.java
+++ app/src/main/java/com/sang/loginwithfirebase/RegisterAccount.java
@@ -1,33 +1,19 @@
 package com.sang.loginwithfirebase;
 
-import androidx.annotation.NonNull;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 import java.util.Objects;
 
 
 public class RegisterAccount extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    ProgressDialog progressDialog;
+    //ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,11 +26,11 @@ public class RegisterAccount extends AppCompatActivity {
 
 
         //Progressing
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Registering your account...");
+//        progressDialog = new ProgressDialog(this);
+//        progressDialog.setMessage("Registering your account...");
 
         //get the shared instance of the FirebaseAuth object
-        mAuth = FirebaseAuth.getInstance();
+
 
         //Click Create an account -> Register account
         register.setOnClickListener(new View.OnClickListener() {
@@ -68,47 +54,11 @@ public class RegisterAccount extends AppCompatActivity {
                     r_password.setFocusable(true);
 
                 }
-                else{
-                    registerUser(email, pass);
-                }
+//                else{
+////                    //registerUser(email, pass);
+////                }
             }
         });
 
     }
-
-    private void registerUser(String email, String  pass) {
-        //Email and password are valid -> progressing and register the user
-        progressDialog.show();
-
-        mAuth.createUserWithEmailAndPassword(email, pass)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, dismiss the dialog and start register activity
-
-                            progressDialog.dismiss();
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(RegisterAccount.this, "Registered",Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(RegisterAccount.this, Hello.class);
-                            startActivity(intent);
-
-                            finish();
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(RegisterAccount.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                //Error, dismiss progress dialog and get and show the error
-                progressDialog.dismiss();
-                Toast.makeText(RegisterAccount.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
 }
